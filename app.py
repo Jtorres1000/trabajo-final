42b73247d88303cf295fc327dccd7ccd0d006359
jtorres1000 <fervirlacrer@gmail.com>
2026-03-13 13:17:00 -0400

    fix: Se aumentó el límite de canciones de 10k a 15k y se quitó el rellenado con a los NA ya que no son relevantes para la investigacion

--- app.py
+++ app.py
@@ -29,13 +29,12 @@ def cargar_csv(path: str) -> pd.DataFrame:
 
 def preprocesar_datos(df: pd.DataFrame) -> pd.DataFrame:
     """Preprocesa el DataFrame de Spotify."""
-    df = df.head(10000).copy()
     df = df.convert_dtypes()
+    df = df.head(15000).copy()
     df = df.replace(r'^\s*$', np.nan, regex=True)
     df["duration_ms"] = df["duration_ms"].div(1000 * 60).round(2)
     df = df.rename(columns={'duration_ms': 'duration_min'})
     df['release_date'] = pd.to_datetime(df['release_date'])
-    df = df.fillna('Unknown')
     
     return df
 
