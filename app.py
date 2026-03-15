6eb858227f1653704e7396cdf6c6cc986740aaab
jtorres1000 <fervirlacrer@gmail.com>
2026-03-15 00:35:57 -0400

    refactor: reestructuración del archivo app.py para usar multipages, se dividen las secciones en funciones y se agrega un nuevo tipo de gráfico, se agregó un reproductor con música y una imagen de fondo principal

--- app.py
+++ app.py
@@ -7,28 +7,25 @@ import plotly.express as px
 import base64
 from streamlit_echarts import st_echarts
 
-
-# Definir una paleta de colores personalizada inspirada en Spotify  
+# Configuración inicial de la app, tema, fuentes, etc.
 
 PALETA_SPOTIFY = [
-    "#1DB954",  # Verde principal
-    "#212121",  # Negro suave
-    "#121212",  # Negro profundo
-    "#535353",  # Gris oscuro
-    "#B3B3B3"   # Gris claro
+"#1DB954",
+"#212121",
+"#121212",
+"#535353",
+"#B3B3B3"
 ]
 
-# Configuración inicial del dashboard
-
 sns.set_palette(sns.color_palette(PALETA_SPOTIFY))
-st.set_page_config(page_title="Spotify Dashboard Análisis de datos", layout="wide")
+st.set_page_config(page_title="Spotify Dashboard", layout="wide", page_icon="🎵")
+
+# Procesamiento, limpieza y carga de datos.
 
 # Función para cargar el dataset
 @st.cache_data
 def cargar_csv(path: str) -> pd.DataFrame:
-    """Carga un archivo CSV desde una ruta dada."""
-    df = pd.read_csv(path)
-    return df
+    return pd.read_csv(path)
 
 def preprocesar_datos(df: pd.DataFrame) -> pd.DataFrame:
     """Preprocesa el DataFrame de Spotify."""
@@ -38,7 +35,6 @@ def preprocesar_datos(df: pd.DataFrame) -> pd.DataFrame:
     df["duration_ms"] = df["duration_ms"].div(1000 * 60).round(2)
     df = df.rename(columns={'duration_ms': 'duration_min'})
     df['release_date'] = pd.to_datetime(df['release_date'])
-    
     return df
 
 # Función para agregar fondo personalizado al sidebar.
@@ -63,10 +59,11 @@ def background_sidebar(image_file: str):
 
 # Carga inicial
 df = cargar_csv("18. Spotify 2015-2025.csv")
-
 df_processed = preprocesar_datos(df)
 
-# Sidebar para filtrado y búsqueda
+
+# Menu lateral y filtros globales
+
 with st.sidebar:
     background_sidebar("imagen/Background 17.png")
     col1, col2, col3 = st.columns([1,1,1])
@@ -76,99 +73,248 @@ with st.sidebar:
     st.audio("imagen/Spotify Wrapped 2025 Background Music (High Quality).mp3", start_time=5, autoplay= True, loop= True)
     st.header("Filtros de búsqueda")
 
-    cities = df_processed['country'].unique()
-    selected_cities = st.multiselect("Selecciona una o más ciudades:", cities, default=["Canada", "Brazil", "Germany"], help="Selecciona uno o más países de la lista para filtrar los datos.", placeholder="Escoge una opción...")
+    cities = df_processed['country'].dropna().unique()
 
-    year_range = st.select_slider("Selecciona un rango de años:", options=sorted(df_processed['release_date'].dt.year.unique()), value=(2015, 2025), help="Selecciona un rango de años para filtrar los datos.")
+    selected_cities = st.multiselect(
+        "Selecciona una o más ciudades:", 
+        cities, 
+        default=["Canada", "Brazil", "Germany"], 
+        help="Selecciona uno o más países.", 
+        placeholder="Escoge una opción..."
+    )
 
-st.title("Dasboard exploratorio de Spotify Data (2015-2025)")
-st.markdown("Análisis exploratorio de datos de Spotify utilizando Streamlit, Pandas, Matplotlib y Seaborn.")
+    años_disponibles = sorted(df_processed['release_date'].dt.year.dropna().unique())
+    year_range = st.select_slider(
+        "Selecciona un rango de años:", 
+        options=años_disponibles, 
+        value=(2015, 2025)
+    )
+    st.markdown("---")
+    st.markdown("##### Acerca de este dashboard:")
+    st.caption("Desarrollado usando Streamlit, pandas y plotly.")
 
+# Aplicar filtros
 df_filtered = df_processed.copy()
-
-# Filtro por Países
 if selected_cities:
     df_filtered = df_filtered[df_filtered['country'].isin(selected_cities)]
 
-# Filtro por Años 2015-2025
 df_filtered = df_filtered[
     (df_filtered['release_date'].dt.year >= year_range[0]) & 
     (df_filtered['release_date'].dt.year <= year_range[1])
 ]
 
-st.write(f"Mostrando {len(df_filtered)} canciones:")
+# Variables globales para los gráficos
+df_filtered['año'] = df_filtered['release_date'].dt.year
 
-with st.expander("Ver datos en formato de tablas filtrados:"):
-    st.markdown("## Datos en tablas filtrados:")
-    st.dataframe(df_filtered)
+duracion_pais_tiempo = df_filtered.groupby(['año', 'country'])['duration_min'].mean().reset_index()
+duracion_pais_tiempo = duracion_pais_tiempo.rename(columns={'country': 'país', 'duration_min': 'duración_promedio'})
+
+distribucion_años = df_filtered[['año', 'duration_min']].sort_values(by='año').reset_index(drop=True)
+media_anual_duracion = distribucion_años.groupby('año')['duration_min'].mean().reset_index(name='duración_promedio')
+
+columnas_corr = ['duration_min', 'popularity', 'tempo', 'energy', 'stream_count']
+columnas_validas = [col for col in columnas_corr if col in df_filtered.columns]
+corr_matrix = df_filtered[columnas_validas].corr()
+
+# Paginas del dashboard
+
+def pagina_inicio():
+    st.title("Dashboard exploratorio de las canciones de Spotify (2015-2025)", anchor=False, text_alignment="center")
+    col1, col2, col3 = st.columns([1, 8, 1])
+    with col2:
+        st.image("imagen/Spotify_New_Full_Logo_RGB_Green.png")
+        st.markdown("### Bienvenido al dashboard interactivo exploratoria de datos de Spotify!", text_alignment="center")
+        st.markdown("Aquí podrás visualizar e interactuar con la data de las canciones de Spotify lanzadas entre 2015 y 2025, cuenta con filtros y gráficos interactivos.", text_alignment="center")
+    st.markdown("---")
+
+    cancion_mas_corta = df_processed.sort_values(by="duration_min").head(1)
+
+    r1, r2, r3 = st.columns([1,1,1])
+    r1.metric("Total de canciones:", f"{len(df_processed)}")
+    r2.metric("Duración promedio:", f"{df_processed['duration_min'].mean():.2f} min")
+    r3.metric("Canción más corta.", f"{cancion_mas_corta["duration_min"].iloc[0]} min")
+    st.markdown(
+    """
+    <div style="background-color: #f0f2f6; padding: 15px; margin-bottom:15px; border-radius: 10px; border-left: 5px solid #1DB954;">
+        <p style="color: #31333F; margin: 0;">
+            Usa el menú de navegación en la parte de arriba para navegar por las distintas páginas y explorar los datos de Spotify.
+        </p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+    st.markdown(
+    """
+    <div style="background-color: #f0f2f6; padding: 15px; margin-bottom:15px; border-radius: 10px; border-left: 5px solid #1DB954;">
+        <p style="color: #31333F; margin: 0;">
+            Usa el menú de la izquierda para filtrar el dataset, a diferencia de los filtros en los gráficos, este remueve la data del dataset original.
+        </p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+def pagina_analisis_duracion():
+    st.title("Análisis de duración 🔎")
+    
+    # Gráfico ECharts
+
+    df_pivot = duracion_pais_tiempo.pivot(index='año', columns='país', values='duración_promedio')
+
+    años, paises = df_pivot.index.tolist(), df_pivot.columns.tolist()
+    
+    series_data = []
+    for pais in paises:
+        valores = df_pivot[pais].where(pd.notnull(df_pivot[pais]), None).tolist()
+        series_data.append({
+            "name": pais, "type": "line", "data": valores,
+            "emphasis": {"focus": "series"}
+        })
+
+    options = {
+    "title": {"text": "Duración Promedio por Año y País"},
+    "tooltip": {"trigger": "axis", "axisPointer": {"type": "cross"}},
+    "legend": {
+        "data": paises,
+        "type": "scroll",
+        "orient": "vertical",
+        "right": "0",
+        "top": "center",
+        "padding": [5, 10]
+    },
+    "grid": {
+        "left": "3%",
+        "right": "15%", 
+        "bottom": "15%",
+        "containLabel": True
+    },
+    "xAxis": {
+        "type": "category", 
+        "boundaryGap": False, 
+        "data": años
+    },
+    "yAxis": {
+        "type": "value", 
+        "name": "Minutos", 
+        "min": 3.5, 
+        "max": 5
+    },
+    "dataZoom": [
+        {
+            "type": "slider", 
+            "xAxisIndex": 0,
+            "bottom": "5%"
+        }, 
+        {
+            "type": "inside", 
+            "xAxisIndex": 0
+        }
+    ],
+    "series": series_data
+}
+    st_echarts(options=options, height="500px", key="pyechart-line-duracion-promedio")
+
+    # Gráfico de Caja Plotly
+    fig_box = px.box(
+        distribucion_años, x='año', y='duration_min', 
+        title="Gráfico de caja y bigotes según el año de lanzamiento y la duración en minutos", 
+        labels={"año": "Año de lanzamiento", 'duration_min': "Duración (minutos)"}, 
+        color_discrete_sequence=PALETA_SPOTIFY
+    )
+    fig_box.update_xaxes(type='category')
+    st.plotly_chart(fig_box, use_container_width=True)
+
+    # Gráfico de barras
+
+    # Convertimos las columnas a listas normales (ECharts no lee Series de Pandas directamente)
+    años_lista = media_anual_duracion["año"].tolist()
+    duraciones_lista = media_anual_duracion["duración_promedio"].tolist()
+    options = {
+    "tooltip": {
+        "trigger": "axis",
+        "axisPointer": {
+            "type": "shadow"
+        }
+    },
+    "grid": {
+        "left": "3%",
+        "right": "4%",
+        "bottom": "3%",
+        "containLabel": True
+    },
+    "xAxis": [
+        {
+            "type": "category",
+            "data": años_lista,       # <--- Tu lista de años aquí
+            "axisTick": {
+                "alignWithLabel": True
+            }
+        }
+    ],
+    "yAxis": [
+        {
+            "type": "value",
+            "name": "Minutos",
+            "min" : 3  
+        }
+    ],
+    "series": [
+        {
+            "name": "Duración",
+            "type": "bar",
+            "barWidth": "60%",
+            "data": duraciones_lista, # <--- Tu lista de duraciones aquí
+            "itemStyle": {
+                "color": "#1DB954"    # Opcional: El verde clásico de Spotify
+            }
+        }
+    ]
+}
+    st_echarts(options=options, height="500px", key="pyechart-bar-duracion")
+
+def pagina_data_tablas():
+    st.title("Data en tablas 📈")
+    st.write(f"**Mostrando {len(df_filtered)} canciones:**")
+
+    st.markdown("### Datos en tablas filtrados:")
+    st.dataframe(df_filtered, use_container_width=True)
 
     st.markdown("### Duración promedio por país y año:")
+    st.dataframe(duracion_pais_tiempo, use_container_width=True)
 
-    duracion_pais_tiempo = df_filtered.groupby([df_filtered['release_date'].dt.year, 'country'])['duration_min'].mean().reset_index()
-    duracion_pais_tiempo = duracion_pais_tiempo.rename(columns={
-    'release_date': 'año',
-    'country': 'país',
-    'duration_min': 'duración_promedio'
-})
-    st.dataframe(duracion_pais_tiempo)
+    st.markdown("### Duración de las canciones agrupadas por año:")
+    st.dataframe(distribucion_años, use_container_width=True)
 
-    duracion_media_año = df_filtered.groupby(df_filtered['release_date'].dt.year)['duration_min'].mean().reset_index()
+    st.markdown("### Duración promedio de las canciones por año")
+    st.dataframe(media_anual_duracion, use_container_width=True)
+
+    st.markdown("### Matriz de correlación de las variables:")
+    st.dataframe(corr_matrix, use_container_width=True)
+
+def pagina_analisis_correlacion():
+    st.title("Análisis de correlación 🔗")
     
-    df_filtered['año'] = df_filtered['release_date'].dt.year
+    if not corr_matrix.empty:
+        fig, ax = plt.subplots(figsize=(10, 8))
+        sns.heatmap(
+            corr_matrix, annot=True, cmap='coolwarm', 
+            linewidths=0.6, fmt='.4f', square=True, ax=ax
+        )
+        ax.set_title('Mapa de Calor - Correlaciones', fontsize=14)
+        st.pyplot(fig, use_container_width=True)
+    else:
+        st.warning("No hay suficientes datos numéricos para calcular la correlación.")
 
-    distribucion_años = df_filtered[['año', 'duration_min']].sort_values(by='año').reset_index(drop=True)
 
-    st.markdown("### Duración promedio por año:")
-    st.dataframe(distribucion_años)
+# Sistema de navegación entre páginas
 
-    columnas_corr = ['duration_min', 'popularity', 'tempo', 'energy', 'stream_count']
+# Convertimos las funciones a objetos st.Page para poder acceder a ellas desde el menú de navegación
 
-    st.markdown("## Matriz de correlación de las variables:")
+p_inicio = st.Page(pagina_inicio, title="Inicio", icon="🏠")
+p_duracion = st.Page(pagina_analisis_duracion, title="Análisis de duración", icon="🔎")
+p_tablas = st.Page(pagina_data_tablas, title="Data en tablas", icon="📈")
+p_correlacion = st.Page(pagina_analisis_correlacion, title="Análisis de Correlación", icon="🔗")
 
-    corr_matrix = df_filtered[columnas_corr].corr()
-
-    st.dataframe(corr_matrix)
-
-# Gráficos
-
-# Gráfico de Caja distribución de años de lanzamiento y duración de las canciones
-fig = px.box(
-    distribucion_años, 
-    x='año', 
-    y='duration_min', 
-    title="Gráfico de caja y bigotes según el año de lanzamiento y la duración en minutos", 
-    labels={"año": "Año de lanzamiento", 'duration_min': "Duración (minutos)"}, 
-    color_discrete_sequence=PALETA_SPOTIFY
-)
-
-fig.update_xaxes(type='category')
-
-st.plotly_chart(fig, use_container_width=True)
-
-fig = px.line(
-    duracion_pais_tiempo, 
-    x='año', 
-    y='duración_promedio', 
-    color='país', 
-    title="Duración promedio por año de lanzamiento según el país.", 
-    labels={
-        "año": "Año de lanzamiento", 
-        'duración_promedio': "Duración promedio (minutos)",
-        "país": "País"
-    }, 
-)
-
-st.plotly_chart(fig, use_container_width=True)
-
-# Gráfico de mapa de calor según las correlaciones entre las variables numéricas    
-fig, ax = plt.subplots(figsize=(10, 8))
-sns.heatmap(corr_matrix, 
-            annot=True, 
-            cmap='coolwarm', 
-            linewidths=0.6,
-            fmt='.4f',
-            square=True,
-            ax=ax)
-ax.set_title('Mapa de Calor - Correlaciones', fontsize=14)
-
-st.pyplot(fig, width='stretch')
+# Creamos el menú y lo ejecutamos
+pg = st.navigation([p_inicio, p_duracion, p_tablas, p_correlacion], position="top")
+pg.run()
