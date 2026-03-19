f33c91ec2a88a5718990a080d4f507d2211f5b52
jtorres1000 <fervirlacrer@gmail.com>
2026-03-19 14:52:05 -0400

    fix: Correcciones de width en los dataframes para quitar el warning en consola

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ def pagina_analisis_duracion():
         color_discrete_sequence=PALETA_SPOTIFY
     )
     fig_box.update_xaxes(type='category')
-    st.plotly_chart(fig_box, use_container_width=True)
+    st.plotly_chart(fig_box, width='stretch')
 
     # Gráfico de barras
 
@@ -306,19 +306,22 @@ def pagina_data_tablas():
     st.write(f"**Mostrando {len(df_filtered)} canciones:**")
 
     st.markdown("### Datos en tablas filtrados:")
-    st.dataframe(df_filtered, use_container_width=True)
+    st.dataframe(df_filtered, width='stretch')
 
     st.markdown("### Duración promedio por país y año:")
-    st.dataframe(duracion_pais_tiempo, use_container_width=True)
+    st.dataframe(duracion_pais_tiempo, width='stretch')
 
     st.markdown("### Duración de las canciones agrupadas por año:")
-    st.dataframe(distribucion_años, use_container_width=True)
+    st.dataframe(distribucion_años, width='stretch')
 
     st.markdown("### Duración promedio de las canciones por año")
-    st.dataframe(media_anual_duracion, use_container_width=True)
+    st.dataframe(media_anual_duracion, width='stretch')
 
     st.markdown("### Matriz de correlación de las variables:")
-    st.dataframe(corr_matrix, use_container_width=True)
+    st.dataframe(corr_matrix, width='stretch')
+
+    st.markdown("### Media de tiempo y energy por año:")
+    st.dataframe(media_tempo_energy, width='stretch')
 
 def pagina_analisis_correlacion():
     st.title("Análisis de correlación 🔗")
