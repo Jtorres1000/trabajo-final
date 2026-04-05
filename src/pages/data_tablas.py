cbba12e28f51a5c2209e744dd3bb048fe8b728bf
jtorres1000 <fervirlacrer@gmail.com>
2026-04-05 02:14:42 -0400

    fix: Se quitó una tabla que  no hacía falta y se filtraron las columnas que no se usaron para el estudio

--- src/pages/data_tablas.py
+++ src/pages/data_tablas.py
@@ -12,13 +12,10 @@ def pagina_data_tablas():
     st.write(f"**Mostrando {len(df_filtered)} canciones:**")
 
     st.markdown("### Datos en tablas filtrados:")
-    st.dataframe(df_filtered, width='stretch')
+    st.dataframe(df_filtered[["release_date", "genre", "duration_min", "popularity", "stream_count", "energy", "country", "explicit", "label", "año"]], width='stretch')
 
     st.markdown("### Duración promedio por país y año:")
     st.dataframe(duracion_pais_tiempo, width='stretch')
-
-    st.markdown("### Duración de las canciones agrupadas por año:")
-    st.dataframe(distribucion_años, width='stretch')
 
     st.markdown("### Duración promedio de las canciones por año")
     st.dataframe(media_anual_duracion, width='stretch')
