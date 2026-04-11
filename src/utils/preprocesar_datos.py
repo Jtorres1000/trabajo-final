01ca9d52682582214d8ad49b0a25af54c01d9f7d
jtorres1000 <fervirlacrer@gmail.com>
2026-04-11 02:59:27 -0400

    feat: Se agregan nuevas conlumnas de segmentación en categorias para tempo y energy

--- src/utils/preprocesar_datos.py
+++ src/utils/preprocesar_datos.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+#Energy min 0.02 energy max 0.99, tempo min 60, tempo max 200 [60-90 lento, 90-120 medio, 120-200 rápido]
+
 def preprocesar_datos(df: pd.DataFrame) -> pd.DataFrame:
     """Preprocesa el DataFrame de Spotify."""
     df = df.convert_dtypes()
@@ -10,5 +12,7 @@ def preprocesar_datos(df: pd.DataFrame) -> pd.DataFrame:
     df = df.rename(columns={'duration_ms': 'duration_min'})
     df['release_date'] = pd.to_datetime(df['release_date'])
     df['año'] = df['release_date'].dt.year
+    df["segmento_tempo"] = pd.cut(df['tempo'], bins=[60, 90, 120, 200], labels=['Lenta', 'Medio', 'Rápida'])
+    df["segmento_energy"] = pd.cut(df['energy'], bins=[0.02, 0.33, 0.66, 0.99], labels=['Baja', 'Media', 'Alta'])
 
     return df
