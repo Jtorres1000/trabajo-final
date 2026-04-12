4f5a495b16aac1681440e173be7229f88849b6a2
jtorres1000 <fervirlacrer@gmail.com>
2026-04-12 14:31:43 -0400

    feat: Se vuelve la rama de development la version con 85k de canciones

--- src/utils/preprocesar_datos.py
+++ src/utils/preprocesar_datos.py
@@ -6,7 +6,6 @@ import numpy as np
 def preprocesar_datos(df: pd.DataFrame) -> pd.DataFrame:
     """Preprocesa el DataFrame de Spotify."""
     df = df.convert_dtypes()
-    df = df.head(15000).copy()
     df = df.replace(r'^\s*$', np.nan, regex=True)
     df["duration_ms"] = df["duration_ms"].div(1000 * 60).round(2)
     df = df.rename(columns={'duration_ms': 'duration_min'})
